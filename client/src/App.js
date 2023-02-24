e1469895301398089c572d3c44702c8044937d93
Naye1443 <76511694+naye1443@users.noreply.github.com>
2023-02-23 20:39:49 -0500

    fix proxy with flask

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 import Home from "./components/Pages/Home";
 function App() {
 
+  const [data, setdata] = useState({  // sets inital states of data
+    Practice: "none"
+  });
+
+  useEffect(() => {
+    fetch("/api").then((res) => res.json().then((data) => {
+      setdata({ // Sets data
+        Practice: data.Practice
+      });
+    })
+    );
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>{data.Practice}</p>
         <Home/>
       </header>
     </div>
